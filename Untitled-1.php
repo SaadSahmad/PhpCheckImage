8da2c450b7bfdbc432e7db522e376e6214e6db89
Saad SAHMAD <saad.sahmad@veepee.com>
2023-11-23 20:00:00 +0100

    first commit

--- Untitled-1.php
+++ Untitled-1.php
@@ -1,4 +1,4 @@
-//Verify if image existe
+//dropDM
 
 <?php
 $image = "tonimage.jpg";
@@ -14,4 +14,45 @@ else
 {
 echo "ton image n'existe pas";
 }
-?>
+{
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "Please select an option:"
+    },
+    "accessory": {
+      "type": "static_select",
+      "placeholder": {
+        "type": "plain_text",
+        "text": "Select an item",
+        "emoji": true
+      },
+      "options": [
+        {
+          "text": {
+            "type": "plain_text",
+            "text": "Option 1",
+            "emoji": true
+          },
+          "value": "option_1"
+        },
+        {
+          "text": {
+            "type": "plain_text",
+            "text": "Option 2",
+            "emoji": true
+          },
+          "value": "option_2"
+        },
+        {
+          "text": {
+            "type": "plain_text",
+            "text": "Option 3",
+            "emoji": true
+          },
+          "value": "option_3"
+        }
+      ]
+    }
+  }
+  
